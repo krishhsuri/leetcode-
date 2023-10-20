e635fb75ac49d9a1f194352609a44d38b016daad
krishhsuri <krish050805@gmail.com>
2023-10-20 18:41:59 +0530

    258

---
+++ 258.add-digits.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=258 lang=python3
+#
+# [258] Add Digits
+#
+
+# @lc code=start
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0 :
+            return 0
+        return 1 + (num-1)%9
+        
+# @lc code=end
+
